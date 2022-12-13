895be03e312bc43a314c809485de212a521e999a
Alex <ajoshua14@icloud.com>
2022-12-12 20:45:44 -0800

    Integrated email api into the contact form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { state, useState } from 'react';
 import './ContactPage.css';
 import Social from '../socialLinks/social';
+import { send } from 'emailjs-com';
 
 export default function ContactPage() {
-    function handleSubmit(e) {
+    const [toSend, setToSend] = useState({
+        name: "",
+        message: "",
+        email: "",
+    });
+
+    const handleChange = (e) => {
+        setToSend((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const handleSubmit = (e) => {
         console.log("Sending message to creator");
+
+        e.preventDefault();
+        send(
+            'service_atisjtd',
+            'template_2hsgujo',
+            toSend,
+            '89GlvH1YRqI53BXU7'
+        )
+            .then((response) => {
+                console.log('Email sent!', response.status, response.text);
+                window.location.reload();
+            }).catch((err) => {
+                console.log('Email failed to send..', err);  
+            });
     }
 
     return (
@@ -14,12 +42,14 @@ export default function ContactPage() {
                     <h1 id="title">Say hey!</h1>
                     <form onSubmit={handleSubmit}>
                         <label id="name">Name</label>
-                        <input type="text" id="name"></input>
+                        <input type="text" name="name" value={toSend["name"]} onChange={handleChange} required></input>
                         
                         <label id="email">Email</label>
-                        <input type="text" id="email"></input>
+                        <input type="text" name="email" value={toSend["email"]} onChange={handleChange} required></input>
+
                         <label id="msgLabel">Message</label>
-                        <textarea type="text" wrap="soft"></textarea>
+                        <textarea type="text" wrap="soft" name="message" value={toSend["message"]} onChange={handleChange} required></textarea>
+
                         <button type="submit" id="send">Send</button>
                     </form>
                 </div>
