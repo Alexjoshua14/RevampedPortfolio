c805b8c0a43155b0a6569651f532da5359f9fbf5
Alex <ajoshua14@icloud.com>
2022-12-13 18:07:19 -0800

    Update style

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,8 +1,36 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 export default function NavBar() {
+    // const [openDrawer, toggleDrawer] = useState(false);
+
+    // useEffect(() => {
+        
+    // })
+
+
+    function displayNav() {
+        console.log("You cannot leave.");
+        var navLinks = document.getElementById("navLinks");
+        var nBlob = document.getElementById("navBlobSmall");
+        if (navLinks.style.display = "none") {
+            console.log("expanding!");
+            navLinks.style.display = "flex";
+            nBlob.display =  "flex";
+            nBlob.width = "75vw";
+            nBlob.background = "var(--dark)";
+            nBlob.color = "var(--white)";
+        } else {
+            nBlob.display =  "flex";
+            nBlob.width = "60px";
+            nBlob.overflow = "hidden";
+            nBlob.background = "var(--dark)";
+            nBlob.color = "var(--white)";
+            navLinks.style.display = "none";
+        }
+    }
+
     return (
         <div className="sticky">
             <Link to="/">
@@ -11,8 +39,20 @@ export default function NavBar() {
             </div>
             </Link>
             <nav>
-                <div className="glassCard" id="navBlob">
+                <div className="glassCard navBlob" id="navBlobLarge">
                     <ul>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/skills">Skills</Link></li>
+                        <li><Link to="/portfolio">Artwork</Link></li>
+                    </ul>
+                </div>
+                <div className="glassCard navBlob" id="navBlobSmall">
+                    <a onClick={displayNav}>
+                        <i className="fa fa-bars" />
+                    </a>
+
+                    <ul id="navLinks">
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                         <li><Link to="/skills">Skills</Link></li>
