a666747504c4cec0acb14dbcbf3341ede15cf540
Alex <ajoshua14@icloud.com>
2022-12-27 21:21:18 -0800

    Update reference from local host to production dev server

--- src/utilities/databaseFunctions.js
+++ src/utilities/databaseFunctions.js
@@ -3,11 +3,11 @@
 import axios from 'axios';
 
 export async function getData() {
-    const response = await axios.get(`http://localhost:5050/api/data`);
+    const response = await axios.get(`https://pure-ridge-29779.herokuapp.com/api/data`);
     return response.data;
 }
 
 export async function getImage(imageKey) {
-    const response = await axios.get(`http://localhost:5050/api/image/${encodeURIComponent(imageKey)}`, { responseType: 'blob'});
+    const response = await axios.get(`https://pure-ridge-29779.herokuapp.com/api/image/${encodeURIComponent(imageKey)}`, { responseType: 'blob'});
     return URL.createObjectURL(response.data);
 }
