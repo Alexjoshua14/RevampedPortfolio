1b4b9aa5a8735522b9eba813267b2e5fd945c612
Alex <ajoshua14@icloud.com>
2023-03-21 16:52:12 -0700

    Fully refresh navigation bar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,32 +6,10 @@ import { TypeAnimation } from'react-type-animation';
 
 export default function HomePage() {
     return (
-        <div className="mainContent">
-            <div className="glassCard" id="welcomeCard">
-                <TypeAnimation 
-                    sequence={[
-                        "Hey there!"
-                    ]}
-                    wrapper="h1"
-                    cursor={false}
-                />
-                <TypeAnimation 
-                    sequence={[
-                        1000,
-                        "I'm Alex"
-                    ]}
-                    wrapper="h2"
-                    cursor={false}
-                    />
-                <TypeAnimation
-                    sequence={[
-                        2000,
-                        "I recently graduated from Williams College with a double major in Computer Science and Studio Art. I'm currently seeking a new Software Engineer role. "
-                    ]}
-                    wrapper="p"
-                    cursor={false}  
-                    speed={60}
-                />
+        <div className="container mainContent">
+            <div className="row" id="main-title">
+                Hey, I'm
+                <br/>Alex Joshua
             </div>
             {/* <div class="selfieHolder">
                 <img src={self} class="selfieImg"></img>
